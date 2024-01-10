7ad47eb5cb0a8a224f0d7d2aca81d916da2c8924
Elliot Snow-Kropla <elliot@ejsk.ca>
2024-01-09 20:30:37 -0400

    Surpress compile warnings in serve script

--- bin/serve.sh
+++ bin/serve.sh
@@ -2,6 +2,6 @@
 
 set -euf -o pipefail
 
-mix generate
+mix generate 2>/dev/null
 caddy file-server --listen :4000 --root public/
 rm -r public/
--- lib/mix/tasks/generate.ex
+++ lib/mix/tasks/generate.ex
@@ -9,8 +9,19 @@ defmodule Mix.Tasks.Generate do
     IO.puts("mkdir -p public")
     File.mkdir_p("public")
 
+    # Create index
+    IO.puts("Writing index.html")
+
     File.open("public/index.html", [:write], fn file ->
       IO.write(file, KsWeb.Templates.index(%{}))
+    end)
+
+    # Create posts
+    IO.puts("Writing posts")
+
+    KsWeb.Templates.posts()
+    |> Enum.each(fn post ->
+      IO.puts("\t#{post.title}")
     end)
   end
 end
