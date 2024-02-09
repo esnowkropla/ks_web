aed2b483556d2d0bb715dba1243eaac969269bbd
Elliot Snow-Kropla <elliot@ejsk.ca>
2024-02-09 14:17:24 -0400

    WIP Stuff

--- bin/serve.sh
+++ bin/serve.sh
@@ -6,7 +6,7 @@ trap 'rm -r public/' SIGINT
 
 mix generate 2>/dev/null
 caddy start
-inotifywait -mI -r -e CLOSE_WRITE templates/ lib/ | while read -r _path _event _file;
+inotifywait -mI -r -e CLOSE_WRITE assets/ templates/ lib/ | while read -r _path _event _file;
 do
   mix generate 2>/dev/null
 done
