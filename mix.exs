d6ade00de47c973994300eb72f3600829f164bad
Elliot Snow-Kropla <elliot@ejsk.ca>
2024-02-11 12:23:23 -0400

    Bump elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule KsWeb.MixProject do
     [
       app: :ks_web,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
