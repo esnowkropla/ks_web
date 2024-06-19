6070aefa9435fd7f4f3730c016f187522be6bafc
Elliot Snow-Kropla <elliot@ejsk.ca>
2024-06-19 12:14:46 -0300

    Pass in a base_dir to generate script

--- lib/mix/tasks/generate.ex
+++ lib/mix/tasks/generate.ex
@@ -1,19 +1,29 @@
 defmodule Mix.Tasks.Generate do
+  @shortdoc "Generate the site"
+
   use Mix.Task
 
-  def run(_) do
+  def run(args) do
     Mix.Task.run("app.start")
+
+    base_dir =
+      case args do
+        [dir | _] -> dir
+        [] -> "public"
+      end
+
+    IO.puts("writing to '#{base_dir}' as base_dir")
 
     # Put the runtime config
     KsWeb.Config.put_toml_config("config.toml")
 
     # Compile templates (this'll happen at compile time? hmm)
     # Generate site in public/
-    IO.puts("mkdir -p public")
-    File.mkdir_p("public")
+    IO.puts("mkdir -p #{base_dir}")
+    File.mkdir_p(base_dir)
 
-    IO.puts("cp -r assets/* public/*")
-    File.cp_r!("assets", "public")
+    IO.puts("cp -r assets/* #{base_dir}/*")
+    File.cp_r!("assets", base_dir)
 
     # Create index
     IO.puts("Writing index.html")
@@ -23,30 +33,30 @@ defmodule Mix.Tasks.Generate do
       build_time: DateTime.utc_now()
     }
 
-    File.open("public/index.html", [:write, :utf8], fn file ->
+    File.open("#{base_dir}/index.html", [:write, :utf8], fn file ->
       IO.write(file, KsWeb.Templates.index(site_assigns))
     end)
 
     # Create projects
-    IO.puts("mkdir -p public/projects")
-    File.mkdir_p!("public/projects")
+    IO.puts("mkdir -p #{base_dir}/projects")
+    File.mkdir_p!("#{base_dir}/projects")
 
-    File.open("public/projects/index.html", [:write, :utf8], fn file ->
+    File.open("#{base_dir}/projects/index.html", [:write, :utf8], fn file ->
       IO.write(file, KsWeb.Templates.projects(site_assigns))
     end)
 
     # Create posts
     IO.puts("Writing posts")
-    IO.puts("mkdir -p public/posts")
-    File.mkdir_p!("public/posts")
+    IO.puts("mkdir -p #{base_dir}/posts")
+    File.mkdir_p!("#{base_dir}/posts")
 
-    File.open("public/posts/index.html", [:write, :utf8], fn file ->
+    File.open("#{base_dir}/posts/index.html", [:write, :utf8], fn file ->
       IO.write(file, KsWeb.Templates.blog(site_assigns))
     end)
 
     KsWeb.Templates.published_posts()
     |> Enum.each(fn post ->
-      post_file = "public/posts/#{post.slug}.html"
+      post_file = "#{base_dir}/posts/#{post.slug}.html"
       IO.puts("\twriting #{post.title} [#{post_file}]")
 
       File.open(post_file, [:write, :utf8], fn file ->
