376ade99acd47981d5a59f38fcc07439d98ba286
Elliot Snow-Kropla <elliot@ejsk.ca>
2024-07-04 16:57:40 -0300

    Update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule KsWeb.MixProject do
     [
       app: :ks_web,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
