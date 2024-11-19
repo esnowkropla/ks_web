0dcdf600afef0b8cb20468df9a14b4892f6f86bb
Elliot Snow-Kropla <elliot@ejsk.ca>
2024-11-19 13:28:48 -0400

    Updated deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 
 echo "Rebuilding site..."
-rm -r "esnowkropla.github.io/*"
+
+pushd esnowkropla.github.io || exit
+rm -r -- *
+popd || exit
+
 mix generate esnowkropla.github.io
-cd esnowkropla.github.io || exit
+
+pushd esnowkropla.github.io || exit
 git add .
 git commit -am "Auto-deploy"
 git push origin master
+sub_hash=$(git rev-parse --short HEAD)
+popd || exit
+git add .
+git commit -am "Deploying $sub_hash in submodule esnowkropla.github.io"
