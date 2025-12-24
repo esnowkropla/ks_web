4a75080d811cd67f8eae2720f879d34880e6e127
Elliot Snow-Kropla <elliot@ejsk.ca>
2025-12-24 10:11:08 -0400

    SQLite post

--- lib/mix/tasks/web/make_post.ex
+++ lib/mix/tasks/web/make_post.ex
@@ -4,36 +4,61 @@ defmodule Mix.Tasks.Web.MakePost do
   use Mix.Task
 
   def run(args) do
-    Mix.Task.run("app.start")
-
-    {switches, [post_title], _} =
-      OptionParser.parse(
-        args,
-        strict: [tags: :string, published: :boolean]
-      )
-
-    tags = get_tags(switches)
-    published = Keyword.get(switches, :published, false)
-    file_name = "#{KsWeb.Sluggify.slug!(post_title)}.md.eex"
-    path = "templates/posts/#{file_name}"
-
-    content = post_template(post_title, tags, published)
-
-    if File.exists?(path) do
-      IO.puts(:stderr, "The file #{path} already exists, aborting")
-      exit(1)
+    if args == [] or "-h" in args or "--help" in args do
+      print_usage()
     else
-      IO.puts("Creating post skeleton ...")
-      IO.puts("title: #{post_title}")
-      IO.puts("tags: #{tags}")
-      IO.puts("path: #{path}")
+      Mix.Task.run("app.start")
 
-      File.open(path, [:write, :utf8], fn file ->
-        IO.write(file, content)
-      end)
+      {switches, [post_title], _} =
+        OptionParser.parse(
+          args,
+          strict: [tags: :string, published: :boolean]
+        )
 
-      IO.puts("done")
+      tags = get_tags(switches)
+      published = Keyword.get(switches, :published, false)
+      file_name = "#{KsWeb.Sluggify.slug!(post_title)}.md.eex"
+      path = "templates/posts/#{file_name}"
+
+      content = post_template(post_title, tags, published)
+
+      if File.exists?(path) do
+        IO.puts(:stderr, "The file #{path} already exists, aborting")
+        exit(1)
+      else
+        IO.puts("Creating post skeleton ...")
+        IO.puts("title: #{post_title}")
+        IO.puts("tags: #{tags}")
+        IO.puts("path: #{path}")
+
+        File.open(path, [:write, :utf8], fn file ->
+          IO.write(file, content)
+        end)
+
+        IO.puts("done")
+      end
     end
+  end
+
+  defp print_usage do
+    IO.puts("""
+    Usage: mix web.make_post [OPTIONS] POST_TITLE
+
+    Generate a new skeleton post with the given title.
+
+    Arguments:
+      POST_TITLE              The title of the post to create
+
+    Options:
+      --tags TAGS             Comma-separated list of tags
+      --published             Mark the post as published (sets published_at)
+      -h, --help              Print this help message
+
+    Examples:
+      mix web.make_post "My First Post"
+      mix web.make_post --tags "elixir,web" "My First Post"
+      mix web.make_post --tags "elixir,web" --published "My First Post"
+    """)
   end
 
   defp post_template(post_title, tags, published) do
